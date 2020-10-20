35fc8c1be57e11a69d86198a7e0a28af602fb515
EndrilsBlackk <nasuscifao9@gmail.com>
2020-10-20 13:33:06 -0300

    primo

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Slides</title>
+</head>
+<body>
+    <!-- slides -->
+    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item">
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="" alt="">
+      <div class="carousel-caption d-none d-md-block">
+      <h5>fasdkmfklamsdflkmadsf</h5>
+      <p>amksdlfmlkasdmfkla</p>
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="" alt="">
+      <div class="carousel-caption d-none d-md-block">
+    <h5>ansdkjfnkjasdnfjk</h5>
+    <p>afjksdnbfjkabsdjfbasd</p>
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="" alt="">
+      <div class="carousel-caption d-none d-md-block">
+    <h5>a\sdmnfklamdslkfmalksdmf</h5>
+    <p>fajksdnfkjansdfkjnasdkjfn</p>
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Anterior</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Próximo</span>
+  </a>
+</div>
+</div>
+</body>
+</html>
